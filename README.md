bbeb3d32ac9b43004f2eedb7708cdeacaaba8a71
Nashwan Abdullah <35005761+codenashwan@users.noreply.github.com>
2024-11-13 13:37:04 +0300

    ^11.9

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Laravel Kurdish Language
 
-Note : Compatible in version 10.10 and up
+Note : Compatible in version ^11.9 and up
 
 - go to `lang` folder
 - create new folder called `ckb`
--- lang/ckb/pagination.php
+++ lang/ckb/pagination.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    
+
     /*
     |--------------------------------------------------------------------------
-    | Pagination Language Lines
+    | دێڕەکانی پەڕەبەندی
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used by the paginator library to build
-    | the simple pagination links. You are free to change them to anything
-    | you want to customize your views to better match your application.
+    | ئەم دێڕانەی خوارەوە بەکاردێن لەلایەن کتێبخانەی پەڕەبەندی بۆ دروستکردنی
+    | بەستەرەکانی پەڕەبەندی سادە. ئازادیت لە گۆڕینیان بۆ هەر شتێک
+    | کە دەتەوێت بۆ ئەوەی ڕووکارەکانت باشتر لەگەڵ بەرنامەکەت بگونجێن.
     |
     */
-    
-    'previous' => '&laquo; پێشتر',
+
+    'previous' => '&laquo; پێشوو',
     'next' => 'دواتر &raquo;',
 
 ];
